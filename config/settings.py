127db81dad7fd113f5dec667b214db4f5550ef18
solohad1n <fox663399@gmail.com>
2026-02-04 10:15:47 +0600

    update

--- config/settings.py
+++ config/settings.py
@@ -44,12 +44,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'note',
-    'api'
+    'api',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -157,3 +159,5 @@ SPECTACULAR_SETTINGS = {
     ],
     'COMPONENT_SPLIT_REQUEST': True,
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
