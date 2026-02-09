f7a1f4306c5c60ba6b95d88c40f2d3f283624937
solohad1n <fox663399@gmail.com>
2026-02-09 11:56:02 +0600

    update

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ if not DEBUG:
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get('NAME'),
-            'USER': os.environ.get('USER'),
+            'USER': os.environ.get('DBUSER'),
             'PASSWORD': os.environ.get('PASSWORD'),
             'HOST': 'localhost',
             'PORT': '5432'
