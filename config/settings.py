7a22e1607a9f0525d3cb004e472542a7b3b1ce87
solohad1n <fox663399@gmail.com>
2026-02-09 11:41:04 +0600

    update

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,17 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+if not DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('NAME'),
+            'USER': os.environ.get('USER'),
+            'PASSWORD': os.environ.get('PASSWORD'),
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
+    }
 
 
 # Password validation
